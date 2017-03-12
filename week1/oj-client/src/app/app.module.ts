c172e585c642013a0fc353b1a7024bb3d9a86710
your name <your_email@youremail.com>
2017-03-12 22:55:18 +0800

    Angular Client Init

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -3,18 +3,33 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { routing } from "./app.routes";
+
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+
+import { DataService } from "./services/data.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent,
+    ProblemListComponent,
+    ProblemListComponent,
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [{
+    provide: "data",
+    useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
